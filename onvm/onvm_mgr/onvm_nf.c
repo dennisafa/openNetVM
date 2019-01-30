fde18460e42f0dbd2e5d857108f05542076efceb
Dennis Afanasev <dennis.afa@gmail.com>
2019-01-30 12:54:03 -0500

    Update onvm_nf.c

--- onvm/onvm_mgr/onvm_nf.c
+++ onvm/onvm_mgr/onvm_nf.c
@@ -191,7 +191,7 @@ onvm_nf_start(struct onvm_nf_info *nf_info) {
 
         if (nf_info->service_id >= MAX_SERVICES) {
         // Maximum service ID exceeded
-                nf_info->status = NF_NO_IDS;
+                nf_info->status = NF_MAX_SERVICES;
                 return 1;
         }
 
